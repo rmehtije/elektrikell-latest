3f6d0be97ef5af3026e020188d4fc357b1face95
rmehtije <rmehtije@gmail.com>
2024-01-24 21:06:50 +0200

    ui react-bootstrap & activePrice

---
+++ src/Head/constants.js
@@ -0,0 +1,12 @@
+export const PRICE_BUTTONS = [
+  {
+    name: "Low price",
+    id: "low",
+  },
+  {
+    name: "High price",
+    id: "high",
+  },
+];
+
+export const DEFAULT_ACTIVE_BUTTON = "low";
