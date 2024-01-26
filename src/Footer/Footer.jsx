2e4e2f58190c7f09b6dbcc442d456438de1c4e84
rmehtije <rmehtije@gmail.com>
2024-01-26 18:24:53 +0200

    depending on activePrice TargetLow TargetHigh

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,13 @@
-function Footer(){
-    return (<></>);
+import TargetHigh from "./TargetHigh";
+import TargetLow from "./TargetLow";
+import { DEFAULT_ACTIVE_BUTTON } from "../Head";
+
+function Footer({ activePrice }) {
+  return (
+    <>
+      {activePrice === DEFAULT_ACTIVE_BUTTON ? <TargetLow /> : <TargetHigh />}
+    </>
+  );
 }
 
 export default Footer;
