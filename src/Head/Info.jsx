8149168462de7a5bb0fe0a2bf8a437215184636b
rmehtije <rmehtije@gmail.com>
2024-01-31 18:54:30 +0200

    currentPrice, add tax

--- src/Head/Info.jsx
+++ src/Head/Info.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { PRICE_BUTTONS, BADGES } from "./constants";
 import Badge from "react-bootstrap/Badge";
+import { getCurrentPrice } from "../services/apiService";
+import { mwToKw, addTax } from "../utils/priceFormats";
 
 function Info({ activePrice, setActivePrice }) {
+  const [currentPrice, setCurrentPrice] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await getCurrentPrice();
+
+      setCurrentPrice(addTax(mwToKw(data[0].price), "ee"));
+    })();
+  }, []);
+
   return (
     <>
       <Col>
@@ -26,7 +39,7 @@ function Info({ activePrice, setActivePrice }) {
         </ButtonGroup>
       </Col>
       <Col className="text-end">
-        <h2>XX.XX</h2>
+        <h2>{currentPrice}</h2>
         <div>cent / kilowatt-hour</div>
       </Col>
     </>
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const VAT = {
+    estonia: 22,
+}; // %
+
+export const COUNTRY_CODES = {
+    ee: 'estonia',
+};
