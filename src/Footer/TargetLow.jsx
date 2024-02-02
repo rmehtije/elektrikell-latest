23811ad5096c145ecf6218179fb6692470bfe2b3
rmehtije <rmehtije@gmail.com>
2024-02-02 18:31:42 +0200

    react-countdown

--- src/Footer/TargetLow.jsx
+++ src/Footer/TargetLow.jsx
@@ -1,8 +1,11 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Intervals from "./Intervals";
+import Countdown from "react-countdown";
+import { addHourToCurrentTSMl } from "../utils/dates";
 
 function TargetLow(props) {
+  const countDownDate = addHourToCurrentTSMl();
   return (
     <>
       <Row>
@@ -14,7 +17,11 @@ function TargetLow(props) {
         </Col>
       </Row>
       <Row>
-        <Col></Col>
+        <Col>
+          <Countdown date={countDownDate}>
+            <div>The time is now!</div>
+          </Countdown>
+        </Col>
       </Row>
     </>
   );
