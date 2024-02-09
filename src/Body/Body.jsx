bf0a15e302919266a0e9e383371c5738c8fe9bb0
rmehtije <rmehtije@gmail.com>
2024-02-09 18:17:14 +0200

    Loading

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -21,7 +21,14 @@ import { getAveragePrice } from "../utils/maths";
 import lodash from "lodash";
 import { ERROR_MESSAGE } from "./constants";
 
-function Body({ from, until, activeHour, setErrorMessage, setBestUntil }) {
+function Body({
+  from,
+  until,
+  activeHour,
+  setErrorMessage,
+  setBestUntil,
+  setIsLoading,
+}) {
   const [priceData, setPriceData] = useState([]);
   const [x1, setX1] = useState(0);
   const [x2, setX2] = useState(0);
@@ -51,8 +58,9 @@ function Body({ from, until, activeHour, setErrorMessage, setBestUntil }) {
 
         setPriceData(priceData);
       })
-      .catch(() => setErrorMessage(ERROR_MESSAGE));
-  }, [from, until, setErrorMessage]);
+      .catch(() => setErrorMessage(ERROR_MESSAGE))
+      .finally(() => setIsLoading(false));
+  }, [from, until, setErrorMessage, setIsLoading]);
 
   useEffect(() => {
     const lowPriceIntervals = getLowPriceInterval(priceData, activeHour);
